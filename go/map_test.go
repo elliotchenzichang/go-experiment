5e9c4ee793bd9408033ee3d87169ddef2d5dafcf
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2023-02-06 21:58:28 +0800

    add delet item in map test

--- go/map_test.go
+++ go/map_test.go
@@ -1,6 +1,21 @@
 package _go
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMap(t *testing.T) {
+}
+
+func TestMapDelete(t *testing.T) {
+	var data = map[string]string{
+		"name": "Elliot",
+	}
+	fmt.Println(data["name"])
+
+	delete(data, "name")
+
+	fmt.Println(data["name"])
+
 }
