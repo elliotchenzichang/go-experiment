38c4d2732fa9bfc8e1f62f5c4f2a7551685e0bd1
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2023-04-16 14:59:39 +0800

    add the performance comparation of map growth

--- go/map_test.go
+++ go/map_test.go
@@ -5,9 +5,6 @@ import (
 	"testing"
 )
 
-func TestMap(t *testing.T) {
-}
-
 func TestMapDelete(t *testing.T) {
 	var data = map[string]string{
 		"name": "Elliot",
@@ -18,4 +15,26 @@ func TestMapDelete(t *testing.T) {
 
 	fmt.Println(data["name"])
 
+}
+
+func BenchmarkPerformanceOfGrowthMapOrNot(b *testing.B) {
+	lengths := []int{1000, 10000, 100000}
+	for _, length := range lengths {
+		b.Run(fmt.Sprintf("test the performance of the ungrow map with the length of %d", length), func(b *testing.B) {
+			m := make(map[string]string, length)
+			for i := 0; i < length; i++ {
+				key := fmt.Sprintf("test_key_%d", i)
+				value := fmt.Sprintf("value_%d", i)
+				m[key] = value
+			}
+		})
+		b.Run(fmt.Sprintf("test the performance of the grow map with the length of %d", length), func(b *testing.B) {
+			m := make(map[string]string)
+			for i := 0; i < length; i++ {
+				key := fmt.Sprintf("test_key_%d", i)
+				value := fmt.Sprintf("value_%d", i)
+				m[key] = value
+			}
+		})
+	}
 }
