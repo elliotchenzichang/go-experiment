440277d6cc135b1400446745dc2051daec410dad
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-01-01 21:49:39 +0800

    add generic type tests

--- go/generic_test.go
+++ go/generic_test.go
@@ -27,3 +27,38 @@ func max[T Numeric](a, b T) bool {
 func TestGeneric_Method(t *testing.T) {
 	max(1, 2)
 }
+
+type Slice[T int | float32 | float64] []T
+
+func TestBasicGenericTypeDefinition(t *testing.T) {
+	var a Slice[int] = []int{1, 2, 3}
+	var b Slice[float32] = []float32{1.1, 2.2, 3.3}
+	var c Slice[float64] = []float64{1.1, 2.2, 3.3}
+	fmt.Println(a[0])
+	fmt.Println(b[0])
+	fmt.Println(c[0])
+}
+
+func TestDefineMapWithGenericType(t *testing.T) {
+	type GenericMap[Key int | string, Value int | string] map[Key]Value
+	var myMap GenericMap[int, string] = map[int]string{
+		1: "Elliot",
+		2: "Chen",
+	}
+	fmt.Println(myMap[1])
+}
+
+type StructureWithGenericType[T int64 | string] struct {
+	Data T
+}
+
+func (f *StructureWithGenericType[T]) Print() {
+	fmt.Println(f.Data)
+}
+
+func TestDefineStructureWithGenericType(t *testing.T) {
+	var obj = &StructureWithGenericType[int64]{
+		Data: 3,
+	}
+	obj.Print()
+}
